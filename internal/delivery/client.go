5539d1c60e109755f30e4e6d104761342d01a842
1-6 <a1-6@MacBook-Pro.local>
2026-08-19 22:48:53 +0800

    fix: apply gold patch for bug 002

--- internal/delivery/client.go
+++ internal/delivery/client.go
@@ -3,11 +3,19 @@ package delivery
 import (
 	"context"
 	"net/http"
+	"time"
 )
 
 type Client struct{ HTTP *http.Client }
 
-func New() *Client { return &Client{} }
+func New() *Client {
+	return &Client{
+		HTTP: &http.Client{
+			Transport: http.DefaultTransport,
+			Timeout:   30 * time.Second,
+		},
+	}
+}
 func (c *Client) Do(ctx context.Context, req *http.Request) (*http.Response, error) {
 	return c.HTTP.Do(req.WithContext(ctx))
 }
